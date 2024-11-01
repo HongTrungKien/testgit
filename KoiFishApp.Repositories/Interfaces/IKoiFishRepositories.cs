d671b41c3855c13ba33b93bf549997aa4a5ef3b1
Hong Trung Kien <kienht8890@ut.edu.vn>
2024-11-01 19:39:18 +0700

    init project

---
+++ KoiFishApp.Repositories/Interfaces/IKoiFishRepositories.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoiFishApp.Repositories.Entities;
+namespace KoiFishApp.Repositories.Interfaces
+{
+    public interface IKoiFishRepositories
+    {
+        Task<List<KoiFish>> GetAllKoiFish();
+    }
+}
---
+++ KoiFishApp.Repositories/KoiFishRepositories.cs
@@ -0,0 +1,23 @@
+﻿using KoiFishApp.Repositories.Entities;
+using KoiFishApp.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace KoiFishApp.Repositories
+{
+    public class KoiFishRepositories : IKoiFishRepositories
+    {
+        private readonly QlcktnContext _DbContext;
+        public KoiFishRepositories(QlcktnContext DbContext)
+        {
+            _DbContext = DbContext;
+        }
+        public async Task<List<KoiFish>> GetAllKoiFish()
+        {
+            return await _DbContext.KoiFishes.ToListAsync();
+        }
+    }
+}
