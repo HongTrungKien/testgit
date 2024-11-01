d895428440b53bdd1b5a8d93ed7415d9c0347401
HongTrungKien <kienht8890@ut.edu.vn>
2024-11-01 19:57:37 +0700

    Update Program.cs

--- KoiFishApp.WebApplication/Program.cs
+++ KoiFishApp.WebApplication/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-// DIII
+// DI
 builder.Services.AddDbContext<QlcktnContext>();
 // DI Repositories
 builder.Services.AddScoped<IKoiFishRepositories, KoiFishRepositories>();
