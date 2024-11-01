833dca2e846670e76830d3ff202574f87e9e7231
Hong Trung Kien <kienht8890@ut.edu.vn>
2024-11-01 22:59:25 +0700

    Testloi

---
+++ KoiFishApp.Services/KoiFishServices.cs
@@ -0,0 +1,24 @@
+﻿using KoiFishApp.Repositories.Entities;
+using KoiFishApp.Repositories.Interfaces;
+using KoiFishApp.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishApp.Services
+{
+    public class KoiFishServices : IKoiFishServices
+    {
+        private readonly IKoiFishRepositories _repositories;
+        public KoiFishServices(IKoiFishRepositories repositories)
+        {
+            _repositories = repositories;
+        }
+        public Task<List<KoiFish>> KoiFish()
+        {
+            return _repositories.GetAllKoiFish();
+        }
+    }
+}
