eaf46e3aabb88445ff54ba76931d45395a384027
TrungKien-tikey <hongtrungkien.gr2022@gmail.com>
2024-11-01 19:56:50 +0700

    Update Program.cs

--- KoiFishApp.WebApplication/Program.cs
+++ KoiFishApp.WebApplication/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-// DII
+// DIII
 builder.Services.AddDbContext<QlcktnContext>();
 // DI Repositories
 builder.Services.AddScoped<IKoiFishRepositories, KoiFishRepositories>();
