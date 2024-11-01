a9fb2f53c4ff3a5a0582c8906b3e369072964d7c
Hong Trung Kien <kienht8890@ut.edu.vn>
2024-11-01 21:44:54 +0700

    CodeKoifish

--- KoiFishApp.WebApplication/obj/Debug/net8.0/KoiFishApp.WebApplication.AssemblyInfo.cs
+++ KoiFishApp.WebApplication/obj/Debug/net8.0/KoiFishApp.WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiFishApp.WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45add24e049f568723ffbe55bc904533b4e35536")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiFishApp.WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiFishApp.WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
